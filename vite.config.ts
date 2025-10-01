28061c7f131379684b9e9208bf0228b0920b0942
mrblack193 <93819629+mrblack193@users.noreply.github.com>
2025-10-01 18:55:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Nếu deploy trên Vercel -> base: '/' 
-// Nếu deploy GitHub Pages -> base: '/Rialoppy/'
+// Nếu deploy trên Vercel thì biến môi trường VERCEL=1
+// GitHub Actions thì không có biến này => mặc định là GitHub Pages
 const isVercel = process.env.VERCEL === '1'
 
 export default defineConfig({
   plugins: [react()],
-  base: isVercel ? '/' : '/Rialoppy/',
+  base: isVercel ? '/' : '/Rialoppy/',  // 👈 chỉnh base theo môi trường
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
