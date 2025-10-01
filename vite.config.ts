879c6cff9c9fd9fd1230aa91f501cdd0d28445b4
mrblack193 <93819629+mrblack193@users.noreply.github.com>
2025-10-01 14:42:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/repo-name/' : './',
+  base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/Rialoppy/' : './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
